3d1d41989247da7cb4a9184c5223bd1f7546f94d
Patrick <patrick17silva@hotmail.com>
2022-07-27 21:32:44 -0300

    Implement all components

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Roda a Roda</h1>
+        <p>Clique no botão abaixo para jogar</p>
+        <button>Jogar</button>
+    </div>
+  )
+}
+
+export default StartScreen
